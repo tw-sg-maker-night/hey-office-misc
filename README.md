db5d3b293b86c3b356eb4b8746c26085d9e2fd2e
Col Harris <col.w.harris@gmail.com>
2017-03-15 21:58:39 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hey Office - Misc Features
+
+## TWGuest
+
+Me: What is the twguest password?
+
+Lex: The twguest password is: ****
+
+
+Me: What is the twevent password?
+
+Lex: The twguest password is: ****
---
+++ handler.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var util = require('util')
+var Lex = require('lex-sdk')
+
+function inspect(obj) {
+  return util.inspect(obj, false, null)
+}
+
+var handlers = {
+  'GetPassword': function() {
+    var network = this.event.currentIntent.slots["Network"]
+    var output = this.event.outputDialogMode
+    if(network.indexOf('guest') != -1) {
+      if(output == 'Voice') {
+        this.emit(':tell', 'The t.w. guest password is: '+process.env.TWGUEST_PASSWORD)
+      } else {
+        this.emit(':tell', 'The twguest password is: '+process.env.TWGUEST_PASSWORD)
+      }
+    } else if(network.indexOf('event') != -1) {
+      if(output == 'Voice') {
+        this.emit(':tell', 'The t.w. event password is: '+process.env.TWEVENT_PASSWORD)
+      } else {
+        this.emit(':tell', 'The twevent password is: '+process.env.TWEVENT_PASSWORD)
+      }
+    } else {
+      this.emit(':tell', 'How should I know!?!')
+    }
+  }
+}
+
+module.exports.getPassword = (event, context, callback) => {
+  console.log("Event = " + inspect(event))
+  var lex = Lex.handler(event, context)
+  lex.registerHandlers(handlers)
+  lex.execute()
+};
